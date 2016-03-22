4ad145bc2629c28749a8128d79da71313ce11015
mohamed <lachhab.med.ali@gmail.com>
2016-03-22 14:22:37 +0100

    add bundle email

--- bundle-trusty-webmail/stack-start.sh
+++ bundle-trusty-webmail/stack-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-heat stack-create $1 -f bundle-trusty-webmail.heat.yml -Pkeypair_name=$2
+heat stack-create $1 -f bundle-trusty-webmail.heat.yml -Pkeypair_name=$2 -Pmysql_password=$3 -Ppostfix_admin=$4
