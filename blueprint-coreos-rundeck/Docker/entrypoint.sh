938d0fbc8bf256a22dc0a3c7cf5076c2214d4988
mohamed <lachhab.med.ali@gmail.com>
2017-01-05 18:48:17 +0100

    fix docker rundeck

--- blueprint-coreos-rundeck/Docker/entrypoint.sh
+++ blueprint-coreos-rundeck/Docker/entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-. /etc/rundeck/profile
+ls -l /etc/rundeck
+source /etc/rundeck/profile
 /bin/bash -c "$rundeckd"
+
+export RD_URL=$(awk -F= "/grails.serverURL/ {print \$2}" /etc/rundeck/rundeck-config.properties)
+export RD_USER=admin RD_PASSWORD=admin
+
+rd projects create -p test
